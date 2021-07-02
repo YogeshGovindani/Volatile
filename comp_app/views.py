ee1a0faefe2cc27d10550875bcd7a05ff5f5afeb
YogeshGovindani <yogeshgovindani09@gmail.com>
2021-07-02 20:07:13 +0530

    signup and login added

--- comp_app/views.py
+++ comp_app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import request
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def login(request):
+    if request.POST:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            print(request.user.username)
+            return redirect("/")
+        else:
+            message = "User doesnt exist"
+            return render(request, "login.html", {
+                "message": message
+            })
+    return render(request, "login.html", {
+        "message": ""
+    })
+
+
+def signup(request):
+    if request.POST:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        print(username, email, password)
+        user = User.objects.create_user(
+            username=username, email=email, password=password)
+        return redirect("/login")
+    return render(request, "signup.html")
+
+
+@login_required(login_url="/login")
+def home(request):
+    print(request.user)
+    return render(request, "home.html")
+
+
+# Getting Error
+def logout(reuest):
+    auth_logout(request)
+    return redirect("/login")
